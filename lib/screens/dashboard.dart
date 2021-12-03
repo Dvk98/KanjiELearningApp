94d4e4ae83b3e55bc4e77444dacbc007c3ac7d3b
grumpy23 <gianluca.gencarelli@googlemail.com>
2021-12-03 01:11:07 +0100

    Added Prozent, Level + EP, letzten Kanji groß klein und level

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
@@ -13,19 +14,105 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  late int level = 0;
+  late int maxExp = 1000;
+  late int currentExp = 0;
+  late int lastResult = 55;
+  late int totalKanji = 0;
+  late String lastBigKanji = "近";
+  late String lastSmallKanji = "日";
+  late String lastKanjiLevel = "N2";
+
+
+  int levelUp(){
+    return level++;
+  }
+
+  void gainExp(int exp) {
+    currentExp += currentExp + exp;
+    if(currentExp >= maxExp) {
+      levelUp();
+      currentExp -= maxExp;
+    }
+  }
+
+  void percentageRight(int right, int total) {
+    lastResult = (right ~/ total) * 100;
+  }
+
+  void setLastKanji(String bigKanji, String smallKanji, String kanjiLevel ) {
+    lastBigKanji = bigKanji;
+    lastSmallKanji = smallKanji;
+    lastKanjiLevel = kanjiLevel;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Menu"),
-        elevation: 0,
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [Expanded(child: Text("TODO"))],
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF0099a9),
         ),
-      ),
-    );
+        body: Column(children: <Widget>[
+          Row(
+            children: [
+              Container(
+                color: Colors.black38,
+                margin: EdgeInsets.all(50.0),
+                height: 400,
+                width: 400,
+                child:
+                  Text(
+                      '\n Level ${level} \n\n'
+                          '${currentExp}/${maxExp}EP' ,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontFamily: 'Roboto',
+                        color: Color(0xFF26C6DA),
+                      )
+                  ),
+              ),
+              Container(
+                color: Colors.black38,
+                margin: EdgeInsets.all(50.0),
+                height: 400,
+                width: 400,
+                child:
+                Text(
+                    '\n Results of your last game \n\n'
+                        '${lastResult}%' ,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontFamily: 'Roboto',
+                      color: Color(0xFF26C6DA),
+                    )
+                ),
+              ),
+            ],
+          ),
+          Column(//ROW 2
+              children: [
+                Container(
+                  color: Colors.black38,
+                  margin: EdgeInsets.all(50.0),
+                  height: 400,
+                  width: 400,
+                  child:
+                  Text(
+                      'Your last Kanji\n'
+                      '${lastBigKanji}\n\n'
+                      '${lastSmallKanji}\n\n'
+                      'Level: ${lastKanjiLevel}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontFamily: 'Roboto',
+                        color: Color(0xFF26C6DA),
+                      )
+                  ),
+                ),
+              ]),
+        ]));
   }
 }
